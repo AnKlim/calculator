d5630ff3fc50bae7f163c6b5aa3247623b531eb4
AnKlim <andrewklimenkov@gmail.com>
2024-12-08 15:30:56 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <span>100 + 47 + 2234</span>
+      <h1>165,345</h1>
+      <div className="grid grid-cols-4 gap-4">
+        <button>C или АС</button>
+        <button>Стереть</button>
+        <button>%</button>
+        <button>/</button>
+
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>*</button>
+
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>+</button>
+
+        <button>Доп</button>
+        <button>0</button>
+        <button>,</button>
+        <button>=</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
